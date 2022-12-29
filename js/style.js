12e031d6d9e745d8548dbb94dcf085a3fa6c9fef
toshiya714 <m1no.34g.nhd.to4@gmail.com>
2022-12-30 00:30:57 +0900

    first commit

---
+++ js/style.js
@@ -0,0 +1,30 @@
+function showClock(){
+    var nowTime = new Date();
+    var nowYear     = nowTime.getFullYear();
+    var nowMonth    = nowTime.getMonth();
+    var nowDate     = nowTime.getDate();
+    var nowHour = set2fig(nowTime.getHours());
+    var nowMin  = set2fig(nowTime.getMinutes());
+    var nowSec  = set2fig(nowTime.getSeconds());
+    var msg = nowYear + "/" + nowMonth + "/" + nowDate + " " + nowHour + ":" + nowMin + ":" + nowSec;
+    document.getElementById('RealTime').innerHTML = msg;
+}
+function set2fig(num){
+    if(num < 10){
+        num = "0" + num;
+    }
+    return num;
+}
+
+function changePage(char){
+    if(char == 'login'){
+        console.log('login');
+        window.location.replace('../html/SignUp.html');
+    }
+    else{
+        console.log('signUp');
+        window.location.replace('../html/SignIn.html');
+    }
+}
+
+setInterval('showClock()', 1000);
